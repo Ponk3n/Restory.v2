ec8db870d731be0b6c11d2e39df4a794672f86c4
Robert <thesouse@gmail.com>
2021-04-23 14:48:57 +0200

    Customizing the header and hooking up the wordpress stuff

--- wp-content/themes/restorytheme/header.php
+++ wp-content/themes/restorytheme/header.php
@@ -14,28 +14,28 @@
 <body>
     <div class="main-wrapper">
         <header>
-            <nav class="navbar navbar-expand-lg myCustomNavbar">
+            <nav class="navbar navbar-expand-md sticky-top myCustomNavbar">
                 <a class="navbar-brand" href="#">
                     <img src="/assets/images/White_logo_Restory_group_education.png" height="30" alt="Restory" />
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <i class="fas fa-bars"></i>
+                    <i class="fas fa-bars myIcon"></i>
                 </button>
 
-                <!--<?php
-                    /*wp_nav_menu(
+                <?php
+                wp_nav_menu(
                     array(
                         'menu' => 'primary',
                         'container' => '',
                         'theme_location' => 'primary',
-                        'items_wrap' => '<ul id="" class="">%3$s</ul>'
+                        'items_wrap' => '<ul id="" class="navbar-nav">%3$s</ul>'
                     )
-                );*/
-                    ?> -->
+                );
+                ?>
 
                 <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item active">
+                    <ul class="navbar-nav ">
+                        <li class="nav-item">
                             <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item">
