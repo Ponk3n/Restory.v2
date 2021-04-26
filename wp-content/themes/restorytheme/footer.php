093b29e4093e6ade049866ad03f482145aed6ae6
Robert <thesouse@gmail.com>
2021-04-26 15:04:37 +0200

    Begun work on footer and adjusted width on hamburger items in navbar

--- wp-content/themes/restorytheme/footer.php
+++ wp-content/themes/restorytheme/footer.php
@@ -3,11 +3,11 @@
 <footer>
     <nav class="navbar navbar-expand-md myCustomFooter">
 
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarFooter" aria-controls="navbarFooter" aria-expanded="false" aria-label="Toggle navigation">
             <i class="fas fa-bars myIcon"></i>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarNav">
+        <div class="collapse navbar-collapse" id="navbarFooter">
             <?php
             wp_nav_menu(
                 array(
