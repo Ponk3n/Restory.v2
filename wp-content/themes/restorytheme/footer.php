ee98767dc3090d155004f0f8402a78cc8a1315ad
Robert <thesouse@gmail.com>
2021-04-27 14:31:40 +0200

    Fleshed out footer

--- wp-content/themes/restorytheme/footer.php
+++ wp-content/themes/restorytheme/footer.php
@@ -1,25 +1,47 @@
 <?php wp_footer(); ?>
 
 <footer>
-    <nav class="navbar navbar-expand-md myCustomFooter">
+    <div class="container-fluid">
+        <div class="row myCustomFooter">
+            <div class="col-10 vertical-top d-flex justify-content-around">
 
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarFooter" aria-controls="navbarFooter" aria-expanded="false" aria-label="Toggle navigation">
-            <i class="fas fa-bars myIcon"></i>
-        </button>
 
-        <div class="collapse navbar-collapse" id="navbarFooter">
-            <?php
-            wp_nav_menu(
-                array(
-                    'menu' => 'primary',
-                    'container' => '',
-                    'theme_location' => 'primary',
-                    'items_wrap' => '<ul id="" class="navbar-nav">%3$s</ul>'
-                )
-            );
-            ?>
+
+                <div class="footerList" id="">
+                    <?php
+                    wp_nav_menu(
+                        array(
+                            'menu' => 'footer',
+                            'container' => '',
+                            'theme_location' => 'footer',
+                            'items_wrap' => '<ul id="" class="">%3$s</ul>'
+                        )
+                    );
+                    ?>
+                </div>
+            </div>
+            <div class="col-2 socialMediaWrapper">
+                <div class="row socialMedia">
+                    <div>
+                        <a href="#" alt="instagram">
+                            <i class="fab fa-instagram fa-2x footerIcon"></i>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="#" alt="twitter">
+                            <i class="fab fa-twitter fa-2x footerIcon"></i>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="#" alt="facebook">
+                            <i class="fab fa-facebook-square fa-2x footerIcon"></i>
+                        </a>
+                    </div>
+                </div>
+
+            </div>
         </div>
-    </nav>
+    </div>
 </footer>
 </div>
 </body>
