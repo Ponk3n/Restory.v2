bc0bf6f80e5af8a4359bac6863bdc529f148b9aa
Robert <thesouse@gmail.com>
2021-04-29 13:46:40 +0200

    Added adjustments to hero

--- wp-content/themes/restorytheme/front-page.php
+++ wp-content/themes/restorytheme/front-page.php
@@ -12,8 +12,8 @@
             <img class="d-block w-100" src="https://images.pexels.com/photos/1714208/pexels-photo-1714208.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Third slide">
         </div>
         <div class="carousel-caption d-none d-md-block">
-            <h5>Boka konsult idag</h5>
-            <a href="#" class="button">Boka</a>
+            <h1>Boka konsult idag</h1>
+            <a href="#" class="button">Läs mer</a>
         </div>
     </div>
 </div>
