b8e41952007043b9643f714deb0f791bb5a88758
Robert <thesouse@gmail.com>
2021-05-03 15:51:26 +0200

    Added midsection and card group 2 with accompanying style

--- wp-content/themes/restorytheme/front-page.php
+++ wp-content/themes/restorytheme/front-page.php
@@ -24,9 +24,9 @@
     <!--// Kort grupp 1 \\-->
     <div>
         <div class="row justify-content-center">
-
-            <h2 class="my-3">Våra Tjänster</h2>
-
+            <div class="col text-center">
+                <h2 class="my-3 customColor">Våra Tjänster</h2>
+            </div>
         </div>
         <div class="row row-cols-5 justify-content-center">
             <div class="card myCustomCard">
@@ -86,6 +86,109 @@
     } */
     ?>
 
+</article>
+
+<!--// Mittsektion \\-->
+<div class="col">
+    <div class="row customWell">
+        <div class="col-md-4 offset-md-2 customWellCols">
+            <h2>Om oss</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore labore nisi, est quod accusamus architecto,
+                magni autem molestias fugit et vel blanditiis aliquam sequi corporis! Accusamus mollitia ratione voluptatem nobis.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore labore nisi, est quod accusamus architecto,
+                magni autem molestias fugit et vel blanditiis aliquam sequi corporis! Accusamus mollitia ratione voluptatem nobis.
+            </p>
+            <a href="#" class="button">Läs mer</a>
+        </div>
+        <div class="col-md-4 customWellCols">
+            <img src="https://images.pexels.com/photos/3184430/pexels-photo-3184430.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" class="img-fluid">
+        </div>
+    </div>
+</div>
+
+<!--// Kort grupp 2 \\-->
+<article class="px-3 py-5 p-md-5">
+    <div class="cardGroup2">
+        <div class="row justify-content-center">
+            <div class="col text-center customColor">
+                <h2 class="my-3">Referenser</h2>
+                <p class="headlineBread">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, dolore! Reprehenderit quidem
+                    suscipit dignissimos ab vel ad incidunt fuga veniam. Cum dolores unde saepe doloremque aspernatur ea eaque? Totam, nihil.
+                </p>
+            </div>
+        </div>
+        <div class="row row-cols-4 justify-content-center">
+            <div class="card myCustomCard">
+                <img src="https://images.pexels.com/photos/443383/pexels-photo-443383.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="Card image" class="card-img-top">
+                <div class="card-body">
+                    <h3 class="card-title">Lorem</h3>
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur sequi nemo, adipisci laudantium sint voluptates
+                        quidem obcaecati delectus optio autem commodi dolores fuga veritatis at rerum quam deserunt? Dolores, dicta!
+                    </p>
+                    <a href="#" class="button">Läs mer</a>
+                </div>
+            </div>
+            <div class="card myCustomCard">
+                <img src="https://images.pexels.com/photos/443383/pexels-photo-443383.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="Card image" class="card-img-top">
+                <div class="card-body">
+                    <h3 class="card-title">Lorem</h3>
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur sequi nemo, adipisci laudantium sint voluptates
+                        quidem obcaecati delectus optio autem commodi dolores fuga veritatis at rerum quam deserunt? Dolores, dicta!
+                    </p>
+                    <a href="#" class="button">Läs mer</a>
+                </div>
+            </div>
+            <div class="card myCustomCard">
+                <img src="https://images.pexels.com/photos/443383/pexels-photo-443383.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="Card image" class="card-img-top">
+                <div class="card-body">
+                    <h3 class="card-title">Lorem</h3>
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur sequi nemo, adipisci laudantium sint voluptates
+                        quidem obcaecati delectus optio autem commodi dolores fuga veritatis at rerum quam deserunt? Dolores, dicta!
+                    </p>
+                    <a href="#" class="button">Läs mer</a>
+                </div>
+            </div>
+            <div class="card myCustomCard">
+                <img src="https://images.pexels.com/photos/443383/pexels-photo-443383.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="Card image" class="card-img-top">
+                <div class="card-body">
+                    <h3 class="card-title">Lorem</h3>
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur sequi nemo, adipisci laudantium sint voluptates
+                        quidem obcaecati delectus optio autem commodi dolores fuga veritatis at rerum quam deserunt? Dolores, dicta!
+                    </p>
+                    <a href="#" class="button">Läs mer</a>
+                </div>
+            </div>
+            <div class="card myCustomCard">
+                <img src="https://images.pexels.com/photos/443383/pexels-photo-443383.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="Card image" class="card-img-top">
+                <div class="card-body">
+                    <h3 class="card-title">Lorem</h3>
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur sequi nemo, adipisci laudantium sint voluptates
+                        quidem obcaecati delectus optio autem commodi dolores fuga veritatis at rerum quam deserunt? Dolores, dicta!
+                    </p>
+                    <a href="#" class="button">Läs mer</a>
+                </div>
+            </div>
+            <div class="card myCustomCard">
+                <img src="https://images.pexels.com/photos/443383/pexels-photo-443383.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="Card image" class="card-img-top">
+                <div class="card-body">
+                    <h3 class="card-title">Lorem</h3>
+                    <p class="card-text">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur sequi nemo, adipisci laudantium sint voluptates
+                        quidem obcaecati delectus optio autem commodi dolores fuga veritatis at rerum quam deserunt? Dolores, dicta!
+                    </p>
+                    <a href="#" class="button">Läs mer</a>
+                </div>
+            </div>
+        </div>
+
+    </div>
 </article>
 
 
